4799c706be9f18c51a9e31fbd76361530331804a
NBoar <NBoar@DESKTOP-U79JFG9>
2016-10-24 11:06:14 -0400

    Added the stack interface.

---
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/Stack.java
@@ -0,0 +1,40 @@
+package edu.ncsu.csc216.pack_scheduler.util;
+
+/**
+ * Interface for the stack methods.
+ * @author NBoar
+ *
+ * @param <E>
+ */
+public interface Stack<E> {
+	
+	/**
+	 * Adds an element to the stack at the top.
+	 * @param element The element to add to the stack.
+	 */
+	void push(E element);
+	
+	/**
+	 * Removes an element from the stack at the top.
+	 * @return The element at the top of the stack.
+	 */
+	E pop();
+	
+	/**
+	 * Indicates if the stack is empty or not.
+	 * @return True or False if the stack is empty or not.
+	 */
+	boolean isEmpty();
+	
+	/**
+	 * Indicates the number of elements in the stack.
+	 * @return The size of the stack.
+	 */
+	int size();
+	
+	/**
+	 * Method to set the stack's capacity.
+	 * @param capacity The capacity to set the stack to.
+	 */
+	void setCapacity(int capacity);
+}
