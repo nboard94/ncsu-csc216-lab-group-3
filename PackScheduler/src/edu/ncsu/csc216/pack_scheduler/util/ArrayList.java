70d8dce0a13e405ab14782cfdbdc688d991f25b4
NBoar <NBoar@DESKTOP-U79JFG9>
2016-10-30 20:01:04 -0400

    Add in more qualifiers to throw IndexOutOfBoundsException.

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/ArrayList.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/ArrayList.java
@@ -104,6 +104,10 @@ public class ArrayList<E> extends AbstractList<E> {
 		if (index < 0 || index >= size()) {
 			throw new IndexOutOfBoundsException();
 		}
+		if (list.length == 0) {
+			throw new IndexOutOfBoundsException();
+		}
+		
 		E element = list[index];
 		
 		for(int i = index; i < list.length - 1; i++) {
