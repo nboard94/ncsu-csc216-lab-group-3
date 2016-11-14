71269b43d6d6812a08c036e6795e9eac5af2f0d4
NBoar <NBoar@DESKTOP-U79JFG9>
2016-11-14 11:08:50 -0500

    Added fields from UML to FacultySchedulePanel.

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/ui/FacultySchedulePanel.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/ui/FacultySchedulePanel.java
@@ -1,6 +1,12 @@
 package edu.ncsu.csc216.pack_scheduler.ui;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.border.Border;
+
+import edu.ncsu.csc216.pack_scheduler.user.schedule.FacultySchedule;
 
 public class FacultySchedulePanel extends JPanel {
 
@@ -8,6 +14,34 @@ public class FacultySchedulePanel extends JPanel {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	private JTable tableSchedule;
+	private JTable tableRoll;
+	//private CourseTableModel scheduleTableModel;
+	//private StudentTableModel rollTableModel;
+	private Border lowerEtched;
+	private JScrollPane scrollSchedule;
+	private JScrollPane scrollRoll;
+	private JPanel pnlCourseDetails;
+	private JLabel lblNameTitle;
+	private JLabel lblSectionTitle;
+	private JLabel lblTitleTitle;
+	private JLabel lblInstructorTitle;
+	private JLabel lblCreditsTitle;
+	private JLabel lblMeetingTitle;
+	private JLabel lblEnrollmentCapTitle;
+	private JLabel lblOpenSeatsTitle;
+	private JLabel lblWaitlistTitle;
+	private JLabel lblName;
+	private JLabel lblSection;
+	private JLabel lblTitle;
+	private JLabel lblInstructor;
+	private JLabel lblCredits;
+	private JLabel lblMeeting;
+	private JLabel lblEnrollmentCap;
+	private JLabel lblOpenSeats;
+	private JLabel lblWaitlist;
+	private FacultySchedule schedule;
 
 	
 }
